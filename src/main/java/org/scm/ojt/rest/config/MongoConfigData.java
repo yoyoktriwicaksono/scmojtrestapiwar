393a265d0539d1f731adaa832efb3a3872d0bb44
Yoyok Tri Wicaksono <yoyok.thewicaksono@gmail.com>
2019-01-08 21:30:04 +0700

    update mongoconfig

--- src/main/java/org/scm/ojt/rest/config/MongoConfigData.java
+++ src/main/java/org/scm/ojt/rest/config/MongoConfigData.java
@@ -4,6 +4,7 @@ package org.scm.ojt.rest.config;
  * Created by Yoyok_T on 29/09/2018.
  */
 public interface MongoConfigData {
+    Boolean enable();
     Integer port();
     String host();
     String database();
